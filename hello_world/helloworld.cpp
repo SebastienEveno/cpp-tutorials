e0e33c8ed4f36324601c98b2c1a9e02e71ed6591
Sebastien <sebastien.louis.eveno@gmail.com>
2020-11-20 19:39:17 +0100

    update helloworld

--- hello_world/helloworld.cpp
+++ hello_world/helloworld.cpp
@@ -2,13 +2,6 @@
  
 int main()
 {
-	std::cout << "Enter an integer:" << "\n";
-	int x {};
-	std::cin >> x;
-	if (x == 0)
-	{
-		std::cout << "The value is equal to zero" << '\n';
-	}
-    
-	return 0;
+    std::cout << "Hello World!";
+    return 0;
 }
