2d112b1a5c5dbaf84fbb0ea81c2e4167308d5e7e
Sebastien <sebastien.louis.eveno@gmail.com>
2020-12-07 16:29:41 +0100

    add main.cpp

---
+++ functions/introduction_to_lambdas/quiz/q2/main.cpp
@@ -0,0 +1,38 @@
+// https://www.learncpp.com/cpp-tutorial/introduction-to-lambdas-anonymous-functions/
+
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <string_view>
+ 
+struct Season
+{
+  std::string_view name{};
+  double averageTemperature{};
+};
+
+auto ascending{
+    [](const Season& a, const Season& b)
+    {
+        return a.averageTemperature < b.averageTemperature;
+    }
+};
+ 
+int main()
+{
+  std::array<Season, 4> seasons{
+    { { "Spring", 285.0 },
+      { "Summer", 296.0 },
+      { "Fall", 288.0 },
+      { "Winter", 263.0 } }
+  };
+ 
+  std::sort(seasons.begin(), seasons.end(), ascending);
+  
+  for (const auto& season : seasons)
+  {
+    std::cout << season.name << '\n';
+  }
+ 
+  return 0;
+}
