8f3eeb6a18af93196d98ab7c8de971d4acc2a94f
Sebastien <sebastien.louis.eveno@gmail.com>
2021-03-25 10:53:59 +0100

    add question_3

---
+++ learncpp/chapter_6/question_3/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+bool passOrFail()
+{
+    static int c{ 3 };
+    return (--c >= 0);
+}
+int main()
+{
+	std::cout << "User #1: " << (passOrFail() ? "Pass" : "Fail") << '\n';
+	std::cout << "User #2: " << (passOrFail() ? "Pass" : "Fail") << '\n';
+	std::cout << "User #3: " << (passOrFail() ? "Pass" : "Fail") << '\n';
+	std::cout << "User #4: " << (passOrFail() ? "Pass" : "Fail") << '\n';
+	std::cout << "User #5: " << (passOrFail() ? "Pass" : "Fail") << '\n';
+ 
+	return 0;
+}
